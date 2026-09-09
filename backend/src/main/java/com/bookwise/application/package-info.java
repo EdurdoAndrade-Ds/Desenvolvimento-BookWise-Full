768e3b1f38344167b39b3cf039f6b20165738a25
Eduardo Andrade <game4edu5@gmail.com>
2026-09-08 21:57:39 -0300

    feat(backend): application completa

---
+++ backend/src/main/java/com/bookwise/application/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Camada de aplicacao.
+ *
+ * <p>Orquestra os casos de uso do sistema. Contem os services, DTOs e mappers.
+ * Depende apenas da camada de dominio.
+ */
+package com.bookwise.application;
