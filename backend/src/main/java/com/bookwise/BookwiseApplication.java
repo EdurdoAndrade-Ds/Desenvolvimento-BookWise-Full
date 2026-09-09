f184233519e3b93d1427e532aa066243138d8b19
Eduardo Andrade <game4edu5@gmail.com>
2026-09-08 22:02:51 -0300

    feat(backend): application config

---
+++ backend/src/main/java/com/bookwise/BookwiseApplication.java
@@ -0,0 +1,15 @@
+package com.bookwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ponto de entrada da aplicacao BookWise.
+ */
+@SpringBootApplication
+public class BookwiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookwiseApplication.class, args);
+    }
+}
